19f8db3cd6f857718da343c23249b329aa4a6b2a
Muhammad Azhar <wattooazhar758@gmail.com>
2026-05-12 12:38:33 +0500

    fav change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rfcjgj4e8a4"
+    "revision": "0.oka51oe2l5s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
